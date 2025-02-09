1631642dce0a57626d2748d8e24044ac975cf94b
Diego Vinícius <diego.cavalcante90@gmail.com>
2025-02-09 20:14:20 -0300

    Adicionando funções e alterações nas Classes: Taks e TaskManager

--- src/Task.java
+++ src/Task.java
@@ -1,6 +1,7 @@
 import java.time.LocalDate;
 
 public class Task {
+    private int nextId = 1;
     private int id;
     private String name;
     private LocalDate creationDate;
@@ -10,54 +11,66 @@ public class Task {
     public Task() {
     }
 
-    public Task(int id, String name, LocalDate creationDate, LocalDate dueDate, TaskStatus status) {
+    public Task(String name, int daysToComplete) {
+        this.nextId = nextId++;
         this.id = id;
         this.name = name;
-        this.creationDate = creationDate;
-        this.dueDate = dueDate;
+        this.creationDate = creationDate.now();
+        this.dueDate = dueDate.plusDays(daysToComplete);
         this.status = status;
+    }
+
+    public int getNextId() {
+        return nextId;
+    }
+
+    public void setNextId(int nextId) {
+        this.nextId = nextId;
     }
 
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public LocalDate getCreationDate() {
         return creationDate;
     }
+
     public void setCreationDate(LocalDate creationDate) {
         this.creationDate = creationDate;
     }
+
     public LocalDate getDueDate() {
         return dueDate;
     }
+
     public void setDueDate(LocalDate dueDate) {
         this.dueDate = dueDate;
     }
+
     public TaskStatus getStatus() {
         return status;
     }
+
     public void setStatus(TaskStatus status) {
         this.status = status;
     }
 
     @Override
     public String toString() {
-        return "Task{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", creationDate=" + creationDate +
-                ", dueDate=" + dueDate +
-                ", status=" + status +
-                '}';
+        return id + " | " + name + " | Criado: " + creationDate + " | Finaliza: " + dueDate + " | Status: " + status;
     }
 
 
--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,8 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskManager {
+    private List<Task> tasks;
 
+    public TaskManager() {
+        this.tasks = new ArrayList<>();
+    }
 
+    /*
+    *  TODO: Adicionar uma task
+    * */
+    public void addTask(Task task) {
+        tasks.add(task);
+        System.out.println("Tarefa adicionada: " + task.getName());
+    }
 
+    /*
+    *  TODO: Remover task pelo Id
+    *  Usar decisões e loop
+    * */
+    public void removeTask(int id) {
+        Task taskToRemove = null;
 
+        for (Task task : tasks) {
+            if (task.getId() == id) {
+                taskToRemove = task;
+                break;
+            }
+
+        if (taskToRemove != null) {
+            tasks.remove(taskToRemove);
+            System.out.println("Tarefa removida: " + taskToRemove.getName());
+        } else {
+            System.out.println("Tarefa não encontrada: " + id);
+        }
+
+        }
+    }
+
+    /*
+    * TODO: Listar as tasks
+    *  Decisão
+    * */
+    public void listTasks() {
+        if (tasks.isEmpty()) {
+            System.out.println("Não há tarefas.");
+        } else {
+            System.out.println("\n Lista de Tarefas:");
+            tasks.forEach(System.out::println);
+        }
+    }
 
 
 }
+
